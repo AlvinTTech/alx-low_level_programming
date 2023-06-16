f73ff5ac08cf316e9bb7a0182f092ddbdb128599
Alvin Kiongo <alvinkiongo3@gmail.com>
2023-06-16 09:50:44 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+	if (low != e && low != q)
+	putchar(low);
+	}
+	putchar('\n');
+	return (0);
+
+}
