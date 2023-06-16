fd53085206a772f9de98eecfdb889a2fdc28c710
Alvin Kiongo <alvinkiongo3@gmail.com>
2023-06-16 10:03:47 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
