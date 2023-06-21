be4195c3c92971d122bdbe970ac821bb74b8679f
Alvin Kiongo <alvinkiongo3@gmail.com>
2023-06-21 02:01:27 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_*10 - Make alphabet *10 times
+ * Return: void
+ */
+
+void print_alphabet_*10(void)
+
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
