192424f508591c2f1e4f0fbdfcf4d7ea231914a9
Alvin Kiongo <alvinkiongo3@gmail.com>
2023-06-20 23:12:52 -0700

    -putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_×10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -8,6 +8,6 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add (int, int);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
