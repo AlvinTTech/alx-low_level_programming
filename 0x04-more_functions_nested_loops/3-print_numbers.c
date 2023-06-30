c99cc6beacddae538ba3b0defba25e8a39be050c
Alvin Kiongo <alvinkiongo3@gmail.com>
2023-06-30 09:52:07 -0700

    changes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0-9
- * return: void
- */
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
 
 void print_numbers(void)
 {
@@ -11,7 +12,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c);
+	_putchar(c);
 	}
 	_putchar('\n');
 }
