e8f61bf8a30169b6b3f2cf603ae7f6d2da1f5c5a
Alvin Kiongo <alvinkiongo3@gmail.com>
2023-06-30 09:36:42 -0700

    final

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns 0
+*/
+void print_line(int n)
+{
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
